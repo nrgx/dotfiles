1786b306cc7b32dbb2c5a4c1ea2c9b25728bec10
Alibek Kaparov <alibekkaparov@gmail.com>
2025-07-06 19:34:32 +0500

    Added Void Linux after-install shell script

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+sudo xbps-install -Syu \
+	dwm \
+	dmenu \
+	st \
+	git \
+	zsh \
+	tmux \
+	picom \
+	polybar \
+	xorg \
+	xinit \
+	base-devel \
+	libX11-devel \
+	libXft-devel \
+	libXinerama-devel \
+	freetype-devel \
+	fontconfig-devel \
+
+
