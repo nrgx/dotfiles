2f265cd685929ac8b72e5003a242390e8ab2b6de
Alibek Kaparov <alibekkaparov@gmail.com>
2025-09-19 18:51:51 +0500

    moved from dwm to bspwm, clean project later

---
+++ bspwm-install.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+sudo xbps-install -Syu \
+	base-devel \
+	nvidia \
+	wget \
+	curl \
+	zsh \
+	alacritty \
+	rxvt-unicode \
+	tmux \
+	neovim \
+	git \
+	p7zip \
+	xorg-server \
+	xset \
+	xinit \
+	xrandr \
+	xsetroot \
+	xorg-input-drivers \
+	xf86-input-libinput \
+	xrdb \
+	fontconfig \
+	dejavu-fonts-ttf \
+	noto-fonts-ttf \
+	font-awesome
+	bspwm \
+	sxhkd \
+	rofi \
+	polybar \
+	picom \
+	feh \
+	fastfetch \
+	ripgrep \
+	ffmpeg \
+	os-prober \
+	pipewire \
+	wireplumber \
+	alsa-pipewire \
+	dbus \
+	polkit \
+	yazi \
+	zathura \
+	libvirt \
+	virt-manager \
+	qemu \
+	elogind \
+	firefox
