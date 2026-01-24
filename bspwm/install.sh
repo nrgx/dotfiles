38c148e4402de3d43890f0a4bf6b860a183e8556
Alibek Kaparov <alibekkaparov@gmail.com>
2026-01-24 21:42:27 +0500

    Removed i3 and dwm; working with bspwm only; fixed installation script

--- bspwm/install.sh
+++ bspwm/install.sh
@@ -6,56 +6,20 @@ sudo xbps-install -Syu
 # Allow installation of proprietary software
 sudo xbps-install void-repo-multilib void-repo-nonfree
 
-# Set up time zone
-# sudo ln -sf /usr/share/zoneinfo/Asia/Almaty /etc/localtime
+# Setup time zone
+sudo ln -sf /usr/share/zoneinfo/Asia/Almaty /etc/localtime
 
 # Install necessary software
-sudo xbps-install -Syu \
-	base-devel \
-	sddm \
-    xorg \
-	dbus \
-	polkit \
-	git \
-	zsh \
-	tmux \
-	curl \
-	wget \
-	neovim \
-	alacritty \
-	rxvt-unicode \
-	dejavu-fonts-ttf \
-	noto-fonts-ttf \
-	font-awesome \
-	bspwm \
-	sxhkd \
-	rofi \
-	polybar \
-	picom \
-	feh \
-	conky \
-	dunst \
-	fastfetch \
-	fzf \
-	btop \
-	rtkit \
-	p7zip \
-	ffmpeg \
-	ripgrep \
-	pipewire \
-	os-prober \
-	wireplumber \
-	alsa-pipewire \
-	brightnessctl \
-	yazi \
-    okular \
-	zathura \
-	qemu \
-	libvirt \
-	virt-manager \
-	firefox \
-    lua \
-    luarocks
+sudo xbps-install -Syu base-devel opendoas \
+  sddm xorg dbus polkit \
+  git zsh tmux curl wget alacritty rxvt-unicode neovim lua luarocks \
+  dejavu-fonts-ttf noto-fonts-ttf font-awesome \
+  bspwm sxhkd rofi polybar picom feh conky dunst fastfetch \
+  fzf btop rtkit p7zip ffmpeg ripgrep os-prober \
+  pam_rundir pipewire wireplumber alsa-pipewire brightnessctl \
+  yazi okular zathura \
+  qemu libvirt virt-manager \
+  firefox
 
 # Create services
 sudo ln -s /etc/sv/dbus /var/service
@@ -67,16 +31,22 @@ sudo ln -s /etc/sv/virtlogd /var/service
 sudo ln -s /etc/sv/virtlockd /var/service
 
 # Copy BSPWM dotfiles to ~/.config
-cp -r .config ~/.config
+cp -r .config .xprofile ~
+
+# Setup alsa, pipewire and wireplumber
+mkdir -p ~/.config/pipewire/pipewire.conf.d/
+ln -s /usr/share/examples/wireplumber/10-wireplumber.conf ~/.config/pipewire/pipewire.conf.d
+ln -s /usr/share/examples/pipewire/20-pipewire-pulse.conf ~/.config/pipewire/pipewire.conf.d
+sudo mkdir -p /etc/alsa/conf.d
+sudo ln -s /usr/share/alsa/alsa.conf.d/50-pipewire.conf /etc/alsa/conf.d
+sudo ln -s /usr/share/alsa/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d
 
 # Create home folders
-mkdir -p ~/downloads 
+mkdir -p ~/downloads
 mkdir -p ~/media/pictures/
 mkdir -p ~/media/screenshots/
 mkdir -p ~/projects
 mkdir -p ~/software
 
 # Copy wallpapers
-cp -r ../wallpapers ~/meida/pictures
-cp .xprofile ~
-
+cp -r ../wallpapers ~/media/pictures
--- i3/polybar/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-THEME="nord"
-
-pkill polybar
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-CONFIG_DIR=$(dirname $0)/$THEME/config.ini
-polybar nord -c $CONFIG_DIR &
